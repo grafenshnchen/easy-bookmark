fab92b52ed318752d9d76d4120e013201a998e08
Adrian Graf <95887074+grafenshnchen@users.noreply.github.com>
2022-01-16 21:31:35 +0100

    Darkmode hinzugefügt

---
+++ easy bookmark/easy bookmark/easy bookmark/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("easy bookmark")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("grafenshnchen Software [Adrian Graf]")>
+<Assembly: AssemblyProduct("easy bookmark")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("Adrian Graf")>
+
+<Assembly: ComVisible(True)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("32959008-ac03-48f2-b00c-b68e1fcd93db")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("2.0.0.0")>
+<Assembly: AssemblyFileVersion("2.0.0.0")>
